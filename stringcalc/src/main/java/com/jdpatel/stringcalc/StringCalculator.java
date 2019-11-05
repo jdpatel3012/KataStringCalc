61413c34c8c0e9d65da4273bc029bfdf5059861d
Jignesh Patel <jignesh.patel@discus.solutions>
2019-11-05 15:24:31 +0530

    Development for custom Delimeter

--- stringcalc/src/main/java/com/jdpatel/stringcalc/StringCalculator.java
+++ stringcalc/src/main/java/com/jdpatel/stringcalc/StringCalculator.java
@@ -14,12 +14,50 @@ public class StringCalculator
     public int add(String text) {
     	if (text == null || text.isEmpty()) {
             return 0;
-        }  
-    	int total = 0;
-    	String[] numbers = text.split(",|\\n");  
-    	for (String item : numbers) {
-                total += Integer.parseInt(item);
         }
+    	
+    	String customDelimiter = text.split("\n")[0];
+    	
+    	 if (customDelimiter == null || customDelimiter.isEmpty()) {
+    		 customDelimiter =  ",";
+         }
+    	 else {
+    		 boolean isNumeric = true ;
+    		 try {
+    	            Double.parseDouble(customDelimiter);
+    	        } catch (NumberFormatException nfe) {
+    	        	isNumeric = false;
+    	        }
+    		 
+    		 if (isNumeric) {
+    			 customDelimiter =  ",";	 
+    		 	}
+        	 
+         
+    	 else if (customDelimiter.length() == 1) {
+    		 
+        	 customDelimiter =  customDelimiter.substring(0, 1);;
+         }
+    	 else 
+    	 {
+    		 customDelimiter =  ",";	 
+    	 }
+    		 
+    }
+    	
+    	
+    	int total = 0;
+    	int skipFirstLines =  (customDelimiter == ",") ? 0 : 2 ;
+    	String[] numbers =  (customDelimiter == ",") ? text.split(",|\n") : text.split("\n|" + customDelimiter);   
+    	for (String item : numbers) {
+    		 if (skipFirstLines > 0) {
+                 --skipFirstLines;
+             } else {
+                total += Integer.parseInt(item);
+             }
+        }
+    	
+    	
     	
         return total;
          
