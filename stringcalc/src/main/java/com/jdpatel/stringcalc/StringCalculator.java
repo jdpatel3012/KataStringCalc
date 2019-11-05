e6700aa708750b303d09d73d5f6011ec87388076
Jignesh Patel <jignesh.patel@discus.solutions>
2019-11-05 14:34:57 +0530

    Development for single number

--- stringcalc/src/main/java/com/jdpatel/stringcalc/StringCalculator.java
+++ stringcalc/src/main/java/com/jdpatel/stringcalc/StringCalculator.java
@@ -14,7 +14,8 @@ public class StringCalculator
     public int add(String text) {
     	if (text == null || text.isEmpty()) {
             return 0;
-        }
-    	return -1;
+        }  
+         return (int) Double.parseDouble(text);
+         
     }
 }
