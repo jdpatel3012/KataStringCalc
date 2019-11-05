a323ff3abdd0dce40847ec0465f235612be637db
Jignesh Patel <jignesh.patel@discus.solutions>
2019-11-05 16:15:26 +0530

    Add null string case

--- stringcalc/src/test/java/com/jdpatel/stringcalc/StringCalculatorTest.java
+++ stringcalc/src/test/java/com/jdpatel/stringcalc/StringCalculatorTest.java
@@ -14,12 +14,13 @@ public class StringCalculatorTest {
     public void returnZeroForEmptyString() {
         StringCalculator strCalc = new StringCalculator();
         assertEquals(0, strCalc.add(""));
+        assertEquals(0, strCalc.add(null));
     }
     
     @Test
     public void returnItseftForSingleNumber() {
         StringCalculator strCalc = new StringCalculator();
-        assertEquals(3, strCalc.add("3"));
+        assertEquals(3, strCalc.add("3"));        
     }
     
     @Test
